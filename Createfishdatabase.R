b2466e882dbbea147db44c070b4f34363eaef0e5
Gary Casterline <casterln@berkeley.edu>
2022-02-26 10:07:13 -0800

    Add library(lubridate)

--- Createfishdatabase.R
+++ Createfishdatabase.R
@@ -4,10 +4,10 @@
 setwd("~/Sail/BMASP/Fish study")
 
 # read excel file - "Fishdata-edited.xlsx"
-library(readxl)    
+library(tidyverse)    
 
 fishdata<-read_excel("Fish-edited.xlsx")
-
+fishdata<-read_csv("Fish-edited.csv")
 #see vars
 head(fishdata)
 tail(fishdata)
@@ -19,7 +19,17 @@ names(fishdata)
 
 #Set up tidetables and visitation times
 
-tideM<-read_excel("TidetablesNOAAMarch.xlsx")
+tideM<-read_csv("TidetablesNOAAMarch.csv",
+                col_types = cols(
+                  Date = col_date(format="%m/%d/%Y"),
+                  Day = col_factor(
+                    levels=c("Sun","Mon","Tue","Wed",
+                             "Thu","Fri","Sat"),
+                    ordered=FALSE),
+                  Time= col_time(format=""),
+                  Pred=col_character()
+                ) 
+              )
 head(tideM)
 
 
@@ -34,6 +44,14 @@ class(tideM$Date)
 #weekend
 wknd<-tideM$Day=="Sat" | tideM$Day == "Sun"
 wknd
+
+#weekday
+wkday<- tideM$Day=="Mon" |
+  tideM$Day=="Tue" |
+  tideM$Day=="Wed" |
+  tideM$Day=="Thu" |
+  tideM$Day=="Fri"
+  
     
 #Times     
 time1<-tideM$Time
@@ -43,23 +61,29 @@ class(time1)
 #to transform your date into a standard format. Then, you can use substr to extract the time. 
 #e.g. substr("2013-10-01 01:23:45 UTC", 12, 16) gives you 01:23.
 time1
-time2<-substr(time1, 12, 16) 
-time2
+#time2<-substr(time1, 12, 16) 
+#time2
 
 #need to put cols together for tideM vars,  above3, wknd, wkday, time2
 
 #weekday morn params
-weekmorn <- above3==TRUE & wknd==FALSE & time2 >= 9:00 & time2 < 15:00
+weekmorn <- above3==TRUE & wknd==FALSE & hour(time1) >= 9 & hour(time1) < 15
 
 #weekday afternoon params
-weekpm <- above3==TRUE  & wknd==FALSE & time2 >=15:00 & time2 < 18:00
+weekpm <- above3==TRUE  & wknd==FALSE & hour(time1) >=15 & hour(time1) < 18
 
 #weekend params
-weekend <- above3==TRUE  & wknd==TRUE & time2 >=9:00 & time2 <=18:00
+weekend <- above3==TRUE  & wknd==TRUE & hour(time1) >=9 & hour(time1) <=18
 
-#need to put cols together for tideM vars,  above3, wknd, wkday, time2, weekmorn, weekpm, weekend
+#need to put cols together for tideM vars:
+# above3, wknd, wkday, time1,
+# weekmorn, weekpm, weekend
 #export into new excel file or csv
 
+TideM2export = tibble(above3,wknd,wkday,time=hour(time1),
+       weekmorn,weekpm,weekend)
 
-tideM$Pred
+write_csv(TideM2export,
+          file="TideM.csv"
+)
 
